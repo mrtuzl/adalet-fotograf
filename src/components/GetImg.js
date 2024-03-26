ffaffe467d4940adb343090fac3aebefb457bffd
mrtuzl <mertuzel666@gmail.com>
2024-03-26 11:08:08 +0300

    socialmedia

--- src/components/GetImg.js
+++ src/components/GetImg.js
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
 import React from "react";
 import Image from "./Image";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
+
 
 export default function GetImg() {
     const [images, setImages] = useState([]);
@@ -16,7 +21,7 @@ export default function GetImg() {
 
     return (    
         <div className="container-wrap cards">
-            <div className="row">
+            <div className="row p-3">
                 {
                     images.map((img) => (
                         <div key={img.id} className="col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-3 d-flex justify-content-center">
@@ -24,6 +29,13 @@ export default function GetImg() {
                         </div>
                     ))
                 }
+               <div className="position-fixed bottom-0 end-0 p-0 bg-white shadow-lg rounded m-1 follow">
+                    <div className="border border-1 p-2 d-flex justify-content-center align-items-center"> <span className="fw-bold"> Bizi takip edin </span> </div>
+                    <div className="border border-1 p-2 d-flex justify-content-center align-items-center"> <span className="fw-bold"> <FaPhone className="mx-1"/> 0312 414 63 27 </span> </div>
+                    <div className="border border-1 p-2 d-flex justify-content-center align-items-center"> <span> <FaInstagram color="#833ab4" size={20} className="mx-1"/> @AdaletPGM </span> </div>
+                    <div className="border border-1 p-2 d-flex justify-content-center align-items-center"> <span> <FaTwitter color="#1DA1F2" size={20} className="mx-1"/> @AdaletPGM </span> </div>
+                    <div className="border border-1 p-2 d-flex justify-content-center align-items-center"> <span> <FaFacebook color="#1877F2" size={20} className="mx-1"/> @AdaletPGM </span> </div>
+               </div>
             </div>
         </div>
     )
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FaInstagram } from "react-icons/fa";
 import { FaWhatsapp } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
@@ -6,18 +7,27 @@ import { FcLike } from "react-icons/fc";
 
 
 export default function Modal({ isOpen, selectedImageUrl, altDescription, handleClose, props }) {
-    console.log(props)
+    
+    const[like, setLike] = useState(props.likes);
+    const handleLike = () => {
+        setLike(() => props.likes + 1)
+    }
     return (
         <>
             {isOpen && (
-                <div className="modal fade show" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style={{display: 'inline-block'}}>
+                <div className="modal fade show p-3 m-3" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style={{display: 'inline-block'}}>
                     <div className="modal-dialog modal-xs">
                         <div className="modal-content">
-                            <div className="modal-header p-0">
+                            <div className="modal-header p-0 position-relative">
                                 <div className="text-center w-50 border-end border-1 p-2 social"> <FaWhatsapp color="#25D366" size={30}/> </div>
                                 <div className="text-center w-50 border-end border-start border-1 p-2 social"> <FaTwitter color="#1DA1F2" size={30}/> </div>
                                 <div className="text-center w-50 border-start border-1 p-2 social"> <FaFacebook color="#1877F2" size={30}/> </div>
-                                <button type="button" className="btn-close bg-dark" onClick={handleClose} aria-label="Close"></button>
+                                <button 
+                                    type="button" 
+                                    className="btn-close bg-secondary opacity-100 position-absolute translate-middle border border-dark rounded-circle p-2" 
+                                    onClick={handleClose} 
+                                    aria-label="Close"
+                                ></button>
                             </div>
                             <div className="modal-body p-0">
                                 <img src={selectedImageUrl} alt={altDescription} className="card-img-top img-fluid"/> 
@@ -27,9 +37,9 @@ export default function Modal({ isOpen, selectedImageUrl, altDescription, handle
                                     <p className="fw-bold text-uppercase mx-2"> {props.user.name} </p>
                                     <p> <FaInstagram color="purple"/> {props.user.instagram_username}</p> 
                                 </div>
-                                <div className="d-flex align-items-center justify-content-center"> 
-                                    <button type="button" className="btn btn-dark">Beğen</button>
-                                    <button type="button" className="btn btn-light border border-1 shadow-sm mx-2">  <FcLike className="mx-1" size={20}/> {props.likes} Beğeni </button> 
+                                <div className="d-flex p-2 align-items-center justify-content-center"> 
+                                    <button type="button" className="btn btn-dark" onClick={handleLike}>Beğen</button>
+                                    <button type="button" className="btn btn-light border border-1 shadow-sm mx-2">  <FcLike className="mx-1" size={20}/> {like} Beğeni </button> 
                                 </div>
                             </div>
                         </div>
